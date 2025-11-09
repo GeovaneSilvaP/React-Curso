98dd91b3abfbd0e78704fbf0c576c7cbee0d8294
GeovaneSilvaP <geovanesil45sil@gmail.com>
2025-11-09 10:30:01 -0300

    Projeto

--- 7_Formulário multi step com React JS/vite-project/src/App.jsx
+++ 7_Formulário multi step com React JS/vite-project/src/App.jsx
@@ -1,8 +1,10 @@
 //Components
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
 //Hooks
 import { useForm } from "./hooks/userForm";
@@ -12,7 +14,7 @@ import "./App.css";
 function App() {
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
 
-  const {currentStep, currentComponent} = useForm(formComponents)
+  const {currentStep, currentComponent, changeStep, isLastStep, isFirsStep} = useForm(formComponents)
 
   return (
     <div className="app">
@@ -24,18 +26,27 @@ function App() {
         </p>
       </div>
       <div className="form-container">
-        <p>etapas</p>
-        <form>
+        <Steps correntsStep={currentStep}/>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type="button">
+            {!isFirsStep &&(
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
-            <button type="submit">
+            )}
+            {!isLastStep ? (
+              <button type="submit">
               <span>Avançar</span>
               <GrFormNext />
             </button>
+            ) : (
+              <button type="button">
+              <span>Enviar</span>
+              <FiSend />
+            </button>
+            )}
           </div>
         </form>
       </div>
